76de4ee62c1b623d8513763d9bf70915060693b3
Yogender Garg <yogi079@yahoo.co.in>
2017-01-19 15:43:59 +0530

    adding project

---
+++ TestCocoaPOD/ArchFoundationKit/ArchFoundationKit/ArchFoundationKit.h
@@ -0,0 +1,19 @@
+//
+//  ArchFoundationKit.h
+//  ArchFoundationKit
+//
+//  Created by Yogender Garg on 1/18/17.
+//  Copyright © 2017 Sapient. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ArchFoundationKit.
+FOUNDATION_EXPORT double ArchFoundationKitVersionNumber;
+
+//! Project version string for ArchFoundationKit.
+FOUNDATION_EXPORT const unsigned char ArchFoundationKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ArchFoundationKit/PublicHeader.h>
+
+
---
+++ TestCocoaPOD/ArchFoundationKit/ArchFoundationKitTests/ArchFoundationKitTests.swift
@@ -0,0 +1,36 @@
+//
+//  ArchFoundationKitTests.swift
+//  ArchFoundationKitTests
+//
+//  Created by Yogender Garg on 1/18/17.
+//  Copyright © 2017 Sapient. All rights reserved.
+//
+
+import XCTest
+@testable import ArchFoundationKit
+
+class ArchFoundationKitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
